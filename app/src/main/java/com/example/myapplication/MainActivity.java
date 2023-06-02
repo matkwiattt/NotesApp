430c835a448d36b3c250678771c746b7ad387a06
matkwiattt <mk.matkwiat@gmail.com>
2023-06-02 20:59:48 +0200

    pierwszy commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton logoutBtn;
+    NoteAdapter noteAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+        recyclerView = findViewById(R.id.recycler_view);
+        logoutBtn = findViewById(R.id.logout_btn);
+
+        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this, NoteDetailsActivity.class)));
+        logoutBtn.setOnClickListener((v) -> logOut());
+        setupRecyclerView();
+    }
+
+    void logOut(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Wylogowanie");
+        builder.setMessage("Czy na pewno chcesz się wylogować?");
+        builder.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+        builder.setNegativeButton("Nie", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    void setupRecyclerView(){
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions <Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options, this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
+    }
+}
